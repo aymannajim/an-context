1575481805cfc87591725fb49bc4a7c5eecfe7c1
aymannajim <36258300+aymannajim@users.noreply.github.com>
2022-06-18 14:23:41 +0100

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,4 @@
+let ButtonsData = [];
 let Buttons = [];
 let Button = [];
 
@@ -6,47 +7,54 @@ const OpenMenu = (data) => {
 }
 
 const CloseMenu = () => {
-    $(".button").remove();
-    $(".buttonDisabled").remove();
+    for (let i = 0; i < ButtonsData.length; i++) {
+        let id = ButtonsData[i].id
+        $(".button").remove();
+    }
+    ButtonsData = [];
     Buttons = [];
     Button = [];
 };
 
 const DrawButtons = (data) => {
-    for (let i = 0; i < data.length; i++) {
-        let context = data[i].context ? data[i].context : ""
-        let footer = data[i].footer ? data[i].footer : ""
-        let element = $(`
-            <div class="${data[i].disabled ? "buttonDisabled" : "button"}" id=` + i + `>
-              <div class="header" id=`+ i + `>` + data[i].header + `</div>
-              <div class="context" id=`+ i + `>` + context + `</div>
-              <div class="footer" id=`+ i + `>` + footer + `</div>
-              ${data[i].subMenu && !data[i].disabled ? `<svg class="submenuicon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>` : ""}
+    ButtonsData = data
+    for (let i = 0; i < ButtonsData.length; i++) {
+        let header = ButtonsData[i].header
+        let message = ButtonsData[i].txt
+        let id = ButtonsData[i].id
+        let element
+
+        element = $(`
+            <div class="button" id=`+id+`>
+              <div class="header" id=`+id+`>`+header+`</div>
+              <div class="txt" id=`+id+`>`+message+`</div>
             </div>`
         );
         $('#buttons').append(element);
-        Buttons[i] = element
-        Button[i] = data[i]
+        Buttons[id] = element
+        if (ButtonsData[i].params) {
+            Button[id] = ButtonsData[i].params
+        }
     }
 };
 
-$(document).click(function (event) {
+$(document).click(function(event){
     let $target = $(event.target);
     if ($target.closest('.button').length && $('.button').is(":visible")) {
         let id = event.target.id;
-        if (Button[id].disabled) return;
-        if (!Button[id].event && !Button[id].args) return;
+        if (!Button[id]) return
         PostData(id)
-        document.getElementById('imageHover').style.display = 'none';
     }
 })
 
 const PostData = (id) => {
-    $.post(`https://nh-context/dataPost`, JSON.stringify({ id: id }))
+    $.post(`https://nh-context/dataPost`, JSON.stringify(Button[id]))
+    return CloseMenu();
 }
 
 const CancelMenu = () => {
     $.post(`https://nh-context/cancel`)
+    return CloseMenu();
 }
 
 window.addEventListener("message", (evt) => {
@@ -58,31 +66,16 @@ window.addEventListener("message", (evt) => {
             return OpenMenu(info);
         case "CLOSE_MENU":
             return CloseMenu();
-        case "CANCEL_MENU":
-            return CancelMenu();
         default:
             return;
     }
 })
 
-window.addEventListener("keyup", (ev) => {
-    if (ev.which == 27) {
-        CancelMenu();
-        document.getElementById('imageHover').style.display = 'none';
-    }
-})
 
-window.addEventListener('mousemove', (event) => {
-    let $target = $(event.target);
-    if ($target.closest('.button:hover').length && $('.button').is(":visible")) {
-        let id = event.target.id;
-        if (!Button[id]) return
-        if (Button[id].image) {
-            document.getElementById('image').src = Button[id].image;
-            document.getElementById('imageHover').style.display = 'block';
-        }
+document.onkeyup = function (event) {
+    event = event || window.event;
+    var charCode = event.keyCode || event.which;
+    if (charCode == 27) {
+        CancelMenu();
     }
-    else {
-        document.getElementById('imageHover').style.display = 'none';
-    }
-})
+};
